73d328e2f9aab96ec576b4572c5b267c8a6675c0
batuhan0715 <roba00777@gmail.com>
2022-07-31 22:16:56 +0300

    Assets

---
+++ Assets/Scripts/CollisonHandler.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CollisonHandler : MonoBehaviour
+{
+    [SerializeField] float levelLoadDelay = 1f;
+    [SerializeField] ParticleSystem succesParticles;
+    [SerializeField] ParticleSystem crashParticles;
+
+    bool isTransitioning = false;
+    bool collisionDisabled = false;
+
+    private void Update()
+    {
+        RespondTheDebugKeys();
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (isTransitioning || collisionDisabled) { return; }
+
+        switch(other.gameObject.tag)
+        {
+            case "Friendly":
+                Debug.Log("This thing is friendly");
+                break;
+            case "Finish":
+                StartSuccesSequence();
+                break;
+            default:
+                StartCrashSequence();
+                break;   
+        }
+    }
+    void StartSuccesSequence()
+    {
+        isTransitioning = true;
+        succesParticles.Play();
+        GetComponent<Movement>().enabled = false;
+        Invoke("LoadNextLevel", levelLoadDelay);
+    }
+
+    void StartCrashSequence()
+    {
+        isTransitioning = true;
+        crashParticles.Play();
+        GetComponent<Movement>().enabled = false;
+        Invoke("ReloadLevel", 1f);
+
+    }
+    void LoadNextLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currentSceneIndex + 1;
+        if(nextSceneIndex == SceneManager.sceneCountInBuildSettings)
+        {
+            nextSceneIndex = 0;
+        }
+        SceneManager.LoadScene(nextSceneIndex);
+    }
+    void ReloadLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex);
+    }
+
+    void RespondTheDebugKeys()
+    {
+        if(Input.GetKeyDown(KeyCode.L))
+        {
+            LoadNextLevel();
+        }
+        else if(Input.GetKeyDown(KeyCode.C))
+        {
+            collisionDisabled = !collisionDisabled; // toggle collision
+        }
+    }
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] float mainThrust = 1000;
+    [SerializeField] float rotationThrust = 100;
+    [SerializeField] ParticleSystem mainThrustParticle;
+    Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        InputThrust();
+        InputRotation();
+    }
+
+    void InputThrust()
+    {
+        if(Input.GetKey(KeyCode.Space))
+        {
+            StartThrusting();
+        }
+        else
+        {
+            StopThrusting();
+        }
+    }
+
+    
+
+    private void StartThrusting()
+    {
+        rb.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime);
+        if (!mainThrustParticle.isPlaying)
+        {
+            mainThrustParticle.Play();
+        }
+    }
+
+    private void StopThrusting()
+    {
+        mainThrustParticle.Stop();
+    }
+
+    void InputRotation()
+    {
+        if(Input.GetKey(KeyCode.A))
+        {
+            ApplyRotation(rotationThrust);
+
+        }
+        else if(Input.GetKey(KeyCode.D))
+        {
+            ApplyRotation(-rotationThrust);
+        }
+    }
+
+    void ApplyRotation(float rotateThisFrame)
+    {
+        rb.freezeRotation = true; // For the obstacle bug
+        transform.Rotate(Vector3.forward * rotateThisFrame * Time.deltaTime);
+        rb.freezeRotation = false; // For the obstacle bug
+    }
+}
